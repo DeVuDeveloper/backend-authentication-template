49b23f9573c55aebdb7f37959a67078220a77b0a
VuDej <drvu47@gmail.com>
2022-07-08 11:55:16 +0200

    password reset controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
     Rails.application.routes.url_helpers.auth_confirm_email_url(host: 'localhost:3000', token: verification.token)
     # ADD Email Job with `url` added in "CONFIRM EMAIL" button
   end
+
+  def send_reset_email
+    return unless confirmed?
+
+    verification = UserVerification.create(user_id: id, verify_type: :reset_email)
+    Rails.application.routes.url_helpers.auth_verify_reset_password_email_url(host: 'localhost:3000',
+                                                                              token: verification.token)
+    # ADD Email Job with `url` added in "RESET YOUR EMAIL" button
+  end
 end
